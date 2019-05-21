a117535bb8a5c5347cb028b1c113a75650ecf27a
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2019-05-21 23:31:08 +0900

    Implement a use of Google Maps

--- app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
@@ -1,13 +1,185 @@
 package jp.ac.titech.itpro.sdl.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private final static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private final static int REQ_PERMISSIONS = 1234;
+
+    private TextView infoView;
+    private GoogleMap map;
+
+    private GoogleApiClient apiClient;
+    private FusedLocationProviderClient locationClient;
+    private LocationRequest request;
+    private LocationCallback callback;
+
+    private enum State {
+        STOPPED,
+        REQUESTING,
+        STARTED
+    }
+    private State state = State.STOPPED;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        infoView = findViewById(R.id.info_view);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        request = new LocationRequest();
+        request.setInterval(10000L);
+        request.setFastestInterval(5000L);
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Log.d(TAG, "onLocationResult");
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                    infoView.setText(getString(R.string.latlng_format, ll.latitude, ll.longitude));
+                    if (map != null) {
+                        map.animateCamera(CameraUpdateFactory.newLatLng(ll));
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        if (state != State.STARTED && apiClient.isConnected()) {
+            startLocationUpdate(true);
+        } else {
+            state = State.REQUESTING;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        if (state == State.STARTED) {
+            stopLocationUpdate();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        apiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        Log.d(TAG, "onMapReady");
+        map.moveCamera(CameraUpdateFactory.zoomTo(15f));
+        this.map = map;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        if (state == State.REQUESTING) {
+            startLocationUpdate(true);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    private void startLocationUpdate(boolean reqPermission) {
+        Log.d(TAG, "startLocationUpdate");
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                if (reqPermission) {
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQ_PERMISSIONS);
+                } else {
+                    String text = getString(R.string.toast_requires_permission_format, permission);
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+        locationClient.requestLocationUpdates(request, callback, null);
+        state = State.STARTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode, @NonNull String[] permissions, @NonNull int[] grants) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        switch (reqCode) {
+            case REQ_PERMISSIONS:
+                startLocationUpdate(false);
+                break;
+        }
+    }
+
+    private void stopLocationUpdate() {
+        Log.d(TAG, "stopLocationUpdate");
+        locationClient.removeLocationUpdates(callback);
+        state = State.STOPPED;
     }
 }
